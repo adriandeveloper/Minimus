8c554392dbb3858c89ff74f2da997ff6f062f537
Adrian Thomas <adrian.l.thomas@outlook.com>
2019-08-17 22:17:59 -0500

    adds initial components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { AddComponent } from './pages/add/add.component';
+import { AddCardComponent } from './ui/add-card/add-card.component';
+import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    DetailsComponent,
+    AddComponent,
+    AddCardComponent,
+    WeatherCardComponent
   ],
   imports: [
     BrowserModule,
